4b1d01c2cab0f3776664bbeabef96916c81cedd2
michel sampil <michel.sampil@gmail.com>
2024-08-01 23:40:23 -0300

    adding degree programs

--- frontend/src/components/SignUp.tsx
+++ frontend/src/components/SignUp.tsx
@@ -12,6 +12,28 @@ const SignUp: React.FC = () => {
   const [academicYear, setAcademicYear] = useState("");
   const { login } = useContext(AuthContext)!;
 
+  const ORTDegreePrograms = [
+    "LICENCIATURA EN INFORMATICA",
+    "INGENIERIA ELECTRICA",
+    "INGENIERIA EN ELECTRONICA",
+    "INGENIERIA EN SISTEMAS",
+    "INGENIERIA EN TELECOMUNICACIONES",
+    "LICENCIATURA EN SISTEMAS",
+    "INGENIERIA - POSTGRADO",
+    "INGENIERIA - CARRERA CORTA",
+    "OTRAS",
+  ];
+
+  const UMDegreePrograms = [
+    "INENIERIA EN INFORMATICA",
+    "INGENIERIA TELEMATICA",
+    "INGENIERIA DE DATOS E INTELIGENCIA ARTIFICIAL",
+    "INGENIERIA CIVIL",
+    "INGENIERIA INDUSTRIAL",
+    "CIENCIA DE DATOS PARA NEGOCIOS",
+    "OTRAS",
+  ];
+
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
@@ -49,20 +71,42 @@ const SignUp: React.FC = () => {
           placeholder="Email"
           required
         />
-        <Input
-          type="text"
-          value={degreeProgram}
-          onChange={(e) => setDegreeProgram(e.target.value)}
-          placeholder="Degree Program"
-          required
-        />
-        <Input
-          type="text"
+        <SelectWrapper>
+          <Select
+            value={degreeProgram}
+            onChange={(e) => setDegreeProgram(e.target.value)}
+            required
+          >
+            <option value="" disabled>
+              Degree Program
+            </option>
+            {UMDegreePrograms.map((e) => {
+              return <option value={e}>{e}</option>;
+            })}
+          </Select>
+        </SelectWrapper>
+        {/* <Input
+          type="number"
+          min="1"
           value={academicYear}
           onChange={(e) => setAcademicYear(e.target.value)}
           placeholder="Current Academic Year"
           required
-        />
+        /> */}
+        <SelectWrapper>
+          <Select
+            value={academicYear}
+            onChange={(e) => setAcademicYear(e.target.value)}
+            required
+          >
+            <option value="" disabled>
+              Academic Year
+            </option>
+            {[1, 2, 3, 4, 5].map((e) => {
+              return <option value={e}>{e}</option>;
+            })}
+          </Select>
+        </SelectWrapper>
         <Button type="submit">Sign up</Button>
         <Footer>
           <p>Already have an account?</p>
@@ -114,6 +158,39 @@ const Input = styled.input`
   border-radius: 4px;
   outline: none;
   transition: border-color 0.3s ease;
+
+  &:focus {
+    border-color: ${colors.lightTurquoise};
+  }
+`;
+
+const SelectWrapper = styled.div`
+  position: relative;
+
+  &::after {
+    content: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAUCAMAAACtdX32AAAAdVBMVEUAAAD///8AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAhMdQaAAAAJ3RSTlMAAAECAwQGBwsOFBwkJTg5RUZ4eYCHkJefpaytrsXGy8zW3+Do8vNn0bsyAAAAYElEQVR42tXROwJDQAAA0Ymw1p9kiT+L5P5HVEi3qJn2lcPjtIuzUIJ/rhIGy762N3XaThqMN1ZPALsZPEzG1x8LrFL77DHBnEMxBewz0fJ6LyFHTPL7xhwzWYrJ9z22AqmQBV757MHfAAAAAElFTkSuQmCC);
+    background-position: 100%;
+    font-size: 1rem;
+    top: 15px;
+    right: 5px;
+    position: absolute;
+    pointer-events: none;
+    color: ${colors.blackGray};
+  }
+`;
+
+const Select = styled.select`
+  color: ${colors.coolGray};
+  padding: 0.75rem;
+  font-size: 1rem;
+  border: 1px solid ${colors.coolGray};
+  border-radius: 4px;
+  outline: none;
+  transition: border-color 0.3s ease;
+  appearance: none; /* Remove default arrow */
+  background: none; /* Remove default background */
+  width: 100%;
+  background: white;
 
   &:focus {
     border-color: ${colors.lightTurquoise};
